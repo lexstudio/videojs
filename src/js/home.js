29bfcfa23c75b4935d1bcf2b7d25776420454ac5
Alexander Moreno <alexander.morenoc@outlook.com>
2020-06-29 15:26:35 -0500

    14 asignacion de atributos

--- src/js/home.js
+++ src/js/home.js
@@ -3,21 +3,13 @@
 ;(async function load() {
   //await
 
+  //--------------------------------------Llamada al API----------------------
+
   const getData = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
     return data
   }
-
-  const $form = document.getElementById("form")
-  const $home = document.getElementById("home")
-
-  $form.addEventListener("submit", (event) => {
-    //debugger
-    event.preventDefault() //quita la accion que viene por defecto del submit en este caso no recarga la pagina
-    $home.classList.add("search-active")
-  })
-
   // //llamada a la api con promesas
   // let dramaList = getData(
   //   "https://yts.mx/api/v2/list_movies.json?genre=drama"
@@ -35,10 +27,26 @@
   const animationList = await getData(
     "https://yts.mx/api/v2/list_movies.json?genre=animation"
   )
-  // console.log("Drama List Async", dramaListAsync)
-  // console.log("Action List: ", actionList)
-  // console.log("Animation List: ", animationList)
 
+  //---------------------------------------------------------------------------
+  const $form = document.getElementById("form")
+  const $home = document.getElementById("home")
+  const $featuringContainer = document.getElementById("featuring")
+
+  const $modal = document.getElementById("modal")
+  const $overlay = document.getElementById("overlay")
+  const $hideModal = document.getElementById("hide-modal")
+
+  const $actionContainer = document.querySelector("#action")
+  const $dramaContainer = document.getElementById("drama")
+  const $animationContainer = document.getElementById("animation")
+
+  // document.querySelector('#modal img')
+  const $modalImage = $modal.querySelector("img")
+  const $modalTitle = $modal.querySelector("h1")
+  const $modalDescription = $modal.querySelector("p")
+
+  //----------------------------------------------------------------------------
   const videoItemTemplate = (movie) => {
     return `<div class="primaryPlaylistItem">
       <div class="primaryPlaylistItem-image">
@@ -50,14 +58,33 @@
     </div>`
   }
 
-  const createTemplate = (HTMLString) => {
-    //creamos un document html
-    const html = document.implementation.createHTMLDocument()
-    //agregamos al document creado el Html que esta en string
-    html.body.innerHTML = HTMLString
+  //------------------------------------------------------------------------------
+  //funcion que agrega los atributos a un elemento
+  const setAttributes = ($element, attributes) => {
+    for (const attribute in attributes) {
+      $element.setAttribute(attribute, attributes[attribute])
+    }
+  }
+  $form.addEventListener("submit", (event) => {
+    //debugger
+    event.preventDefault() //quita la accion que viene por defecto del submit en este caso no recarga la pagina
+    $home.classList.add("search-active")
+    //creamos un nuevo elemento dentro del documento
+    const $loader = document.createElement("img")
+    $featuringContainer.classList.remove("hidden")
+    setAttributes($loader, {
+      src: `src/images/loader.gif`,
+      height: 50,
+      width: 50,
+    })
 
-    //devuelve
-    return html.body.children[0]
+    $featuringContainer.append($loader)
+  })
+
+  const createTemplate = (HTMLString) => {
+    const html = document.implementation.createHTMLDocument() //creamos un document html
+    html.body.innerHTML = HTMLString //agregamos al document creado el Html que esta en string
+    return html.body.children[0] //devuelve
   }
 
   const addEventClick = ($element) => {
@@ -67,6 +94,7 @@
     })
   }
 
+  //---------------------------------------------------------------
   const renderMovieList = (list, $container) => {
     $container.children[0].remove() //remueve el logo de cargando que en este momento es lo unico que hay en el container
     //remplazamos actionList.data.movies x list para hacerla mas dinamica y reciba cualquier lista
@@ -78,19 +106,11 @@
     })
   }
 
-  const $actionContainer = document.querySelector("#action")
-  const $dramaContainer = document.getElementById("drama")
-  const $animationContainer = document.getElementById("animation")
   renderMovieList(actionList.data.movies, $actionContainer)
   renderMovieList(dramaList.data.movies, $dramaContainer)
   renderMovieList(animationList.data.movies, $animationContainer)
 
-  const $featuringContainer = document.getElementById("featuring")
-
-  const $modal = document.getElementById("modal")
-  const $overlay = document.getElementById("overlay")
-  const $hideModal = document.getElementById("hide-modal")
-
+  //------------------------------------------------------------------
   const showModal = () => {
     $overlay.classList.add(`active`)
     $modal.style.animation = `modalIn .8s forwards`
@@ -101,8 +121,4 @@
     $modal.style.animation = `modalOut .8s forwards`
   }
   $hideModal.addEventListener("click", hideModal)
-  // document.querySelector('#modal img')
-  const $modalImage = $modal.querySelector("img")
-  const $modalTitle = $modal.querySelector("h1")
-  const $modalDescription = $modal.querySelector("p")
 })()
