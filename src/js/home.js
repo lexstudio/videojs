f4a0613dd7272274053c2434dede870ad6697256
Alexander Moreno <alexander.morenoc@outlook.com>
2020-06-29 10:50:21 -0500

    12 eventos

--- src/js/home.js
+++ src/js/home.js
@@ -1,94 +1,40 @@
-// // console.log("hola mundo!")
-// // const noCambia = "Leonidas"
-
-// // let cambia = "@LeonidasEsteban"
-
-// // function cambiarNombre(nuevoNombre) {
-// //   cambia = nuevoNombre
-// // }
-
-// const getUser = new Promise((todoBien, todoMal) => {
-//   setTimeout(() => {
-//     todoBien(`Estamos felices 3s`)
-//   }, 3000)
-// })
-
-// getUser
-//   .then(function () {
-//     console.log("Todo esta bien en la vidawwwww")
-//   })
-//   .catch(function (message) {
-//     console.log(message)
-//   })
-
-// const getUser2 = new Promise((todoBien, todoMal) => {
-//   setTimeout(() => {
-//     todoBien(`Estamos felices 5s`)
-//   }, 5000)
-// })
-
-// //para enviar varias promesas, recibe un array
-// //se va a lanzar haste que terminen las 2 promesas
-// Promise.race([getUser, getUser2])
-//   .then(function (message) {
-//     console.log(message)
-//   })
-//   .catch(function (message) {
-//     console.log(message)
-//   })
-
-// /*--------------------AJAX------------------------------- */
-// $.ajax("https://randomuser.me/api/", {
-//   method: "GET",
-//   success: function (data) {
-//     console.log(data)
-//   },
-//   error: function (error) {
-//     console.log(error)
-//   },
-// })
-
-// fetch("https://randomuser.me/api/")
-//   .then(function (response) {
-//     console.log(response)
-//     return response.json() //tambien devuelve una promesa
-//   })
-//   .then(function (user) {
-//     console.log("usuario", user.results[0].name.first)
-//   })
-//   .catch(function () {
-//     console.log("algo fallo")
-//   })
-
 //Funciones asincronas V7
 //para autoejecutar una funcion se la encierra entre paraentesisi eso es sugar syntax
 ;(async function load() {
   //await
 
-  async function getData(url) {
+  const getData = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
     return data
   }
+
+  const $form = document.getElementById("form")
+  $form.addEventListener("submit", (event) => {
+    //debugger
+    event.preventDefault() //quita la accion que viene por defecto del submit en este caso no recarga la pagina
+  })
+
   // //llamada a la api con promesas
   // let dramaList = getData(
   //   "https://yts.mx/api/v2/list_movies.json?genre=drama"
   // ).then((data) => console.log("Drama list promise", data))
   //el mismo llamado pero con async await
-  const dramaListAsync = await getData(
+
+  const dramaList = await getData(
     "https://yts.mx/api/v2/list_movies.json?genre=drama"
   )
 
   const actionList = await getData(
     "https://yts.mx/api/v2/list_movies.json?genre=action"
-  )
+  ) //recibimos la lista de peliculas de accion del llamado a la api
 
   const animationList = await getData(
     "https://yts.mx/api/v2/list_movies.json?genre=animation"
   )
-  console.log("Drama List Async", dramaListAsync)
-  console.log("Action List: ", actionList)
-  console.log("Animation List: ", animationList)
+  // console.log("Drama List Async", dramaListAsync)
+  // console.log("Action List: ", actionList)
+  // console.log("Animation List: ", animationList)
 
   const videoItemTemplate = (movie) => {
     return `<div class="primaryPlaylistItem">
@@ -101,25 +47,41 @@
     </div>`
   }
 
+  const createTemplate = (HTMLString) => {
+    //creamos un document html
+    const html = document.implementation.createHTMLDocument()
+    //agregamos al document creado el Html que esta en string
+    html.body.innerHTML = HTMLString
+
+    //devuelve
+    return html.body.children[0]
+  }
+
+  const addEventClick = ($element) => {
+    $element.addEventListener("click", () => {
+      alert(`Aqui estoy`)
+    })
+  }
+  const renderMovieList = (list, $container) => {
+    $container.children[0].remove() //remueve el logo de cargando que en este momento es lo unico que hay en el container
+    //remplazamos actionList.data.movies x list para hacerla mas dinamica y reciba cualquier lista
+    list.forEach((movie) => {
+      const HTMLString = videoItemTemplate(movie)
+      const movieElement = createTemplate(HTMLString) //llamamos a la funcion para agregar los templates
+      $container.append(movieElement) //reemplazamos $actioncontainer x $container para hacerlo mas dinamico
+      addEventClick(movieElement)
+    })
+  }
+
   const $actionContainer = document.querySelector("#action")
   const $dramaContainer = document.getElementById("drama")
   const $animationContainer = document.getElementById("animation")
-
-  // console.log(videoItemTemplate("src/images/covers/bitcoin.jpg", "bitcoin"))
-  actionList.data.movies.forEach((movie) => {
-    const HTMLString = videoItemTemplate(movie)
-    const $html = document.implementation.createHTMLDocument()
-    //
-    $html.body.innerHTML = HTMLString
-    debugger
-    //agrega contenido al final del elemento seleccionado
-    // $actionContainer.append(HTMLString)
-    $actionContainer.appendChild(html.body.children[0])
-    console.log(HTMLString)
-  })
+  renderMovieList(actionList.data.movies, $actionContainer)
+  renderMovieList(dramaList.data.movies, $dramaContainer)
+  renderMovieList(animationList.data.movies, $animationContainer)
 
   const $featuringContainer = document.getElementById("featuring")
-  const $form = document.getElementById("form")
+
   const $home = document.getElementById("home")
 
   const $modal = document.getElementById("modal")
